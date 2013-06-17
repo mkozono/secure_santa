0b84750e37345463e3cf4000f99b72fdad1bb0fc
Michael Kozono <mkozono@gmail.com>
2013-06-17 00:39:50 -0700

    Fix migration.

--- db/migrate/20130617052418_change_user_to_have_giftee_not_gifter.rb
+++ db/migrate/20130617052418_change_user_to_have_giftee_not_gifter.rb
@@ -1,17 +1,25 @@
 class ChangeUserToHaveGifteeNotGifter < ActiveRecord::Migration
   def up
     add_column :users, :giftee_id, :integer
-    User.all.each do |user|
-      user.gifter.giftee_id = user.id
-    end
+    # Reassign by giftee instead of gifter
+    execute <<-SQL
+      update users set giftee_id = (
+        select id from users u
+        where u.gifter_id = users.id
+        )
+    SQL
     remove_column :users, :gifter_id
   end
 
   def down
     add_column :users, :gifter_id, :integer
-    User.all.each do |user|
-      user.giftee.gifter_id = user.id
-    end
+    # Reassign by gifter instead of giftee
+    execute <<-SQL
+      update users set gifter_id = (
+        select id from users u
+        where u.giftee_id = users.id
+        )
+    SQL
     remove_column :users, :giftee_id
   end
 end
