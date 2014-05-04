61cc0985912718de92afd50abadf771edc815194
Michael Kozono <mkozono@gmail.com>
2014-05-03 23:23:09 -0700

    Can claim player with Facebook user.

--- app/models/player.rb
+++ app/models/player.rb
@@ -9,6 +9,8 @@ class Player < ActiveRecord::Base
   belongs_to :gifter, class_name: "Player", foreign_key: "giftee_id"
   has_one :giftee, class_name: "Player", foreign_key: "giftee_id"
 
+  belongs_to :user
+
   validates :name, presence: true, length: { maximum: 400 }
   validates :event, presence: true
   validate :giftee_same_event, if: lambda { giftee.present? }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140504022625) do
+ActiveRecord::Schema.define(version: 20140504054949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,10 @@ ActiveRecord::Schema.define(version: 20140504022625) do
     t.integer  "giftee_id"
     t.string   "uid"
     t.text     "message"
+    t.integer  "user_id"
   end
+
+  add_index "players", ["user_id"], name: "index_players_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
