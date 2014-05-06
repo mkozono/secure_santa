0ef7d8578b593dbc700a1a3a7909bad70399f855
Michael Kozono <mkozono@gmail.com>
2014-05-05 22:27:21 -0700

    Add user spec and remove unused new_with_session override.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,4 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
     end
   end
-
-  def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"], without_protection: true) do |user|
-        user.attributes = params
-        user.valid?
-      end
-    else
-      super
-    end    
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".from_omniauth" do
+    subject { User.from_omniauth(auth_hash) }
+    let(:auth_hash) { Hashie::Mash.new(:provider => 'foo', :uid => 'bar', :info => {:name => 'baz'}) }
+    context "when a matching user exists" do
+      let!(:user) { User.create!(:provider => 'foo', :uid => 'bar') }
+      it "does NOT create a new user" do
+        expect{subject}.to_not change{User.count}
+      end
+      it { should eq user }
+    end
+    context "when a matching user does NOT exist" do
+      it "creates a new user" do
+        expect{subject}.to change{User.count}.by(1)
+      end
+      its(:provider) { should eq 'foo' }
+      its(:uid) { should eq 'bar' }
+      its(:name) { should eq 'baz' }
+    end
+  end
 end
