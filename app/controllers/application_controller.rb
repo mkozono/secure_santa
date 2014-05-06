1c0977050644a5c4bd19790f83f3beb2fc033328
Michael Kozono <mkozono@gmail.com>
2014-05-05 23:14:39 -0700

    Can sign out of Facebook.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  after_filter :store_location
+
+  def store_location
+    if (request.fullpath != "/sign_out" && !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
   def after_sign_in_path_for(resource_or_scope)
     if request.env['omniauth.origin']
       request.env['omniauth.origin']
     end
+  end
+
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 SecureSanta::Application.routes.draw do
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  devise_scope :user do
+    # Create our own sign out route pointing to devise's controller.
+    delete '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   root :to => 'events#new'
 
